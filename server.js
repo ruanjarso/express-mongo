f893de626b998d845e4893c54eca8eafc0724469
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-09 10:35:44 -0300

    install express, create src, create app.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import http from "http";
-
+//import http from "http";
+import app from "./src/app.js";
 const PORT = 3000;
 
-const rotas = {
+/*const rotas = {
     "/": "Curso de Node.js",
     "/livros": "Entrei na rota Livros",
     "/autores": "Entrei na Rotas Autores"
-};
+};*/
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end(rotas[req.url]);
-});
+});*/
 
-server.listen(PORT,() => {
+app.listen(PORT,() => { //mudança de "server" para "app"
     console.log("servidor escutando");
 });
 

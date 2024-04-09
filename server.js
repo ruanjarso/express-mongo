298a4d91d1063e092db9e05ac4826c561614851d
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-09 10:52:23 -0300

    create route /livros

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-//import http from "http";
+//import http from "http";  //já não é mais preciso ultilizar essa importação pois foi substituida pelo express;
 import app from "./src/app.js";
 const PORT = 3000;
 
-/*const rotas = {
+/*const rotas = {       //já não é mais preciso ultilizar essa criação de rotas pois foi substituido pelo express;
     "/": "Curso de Node.js",
     "/livros": "Entrei na rota Livros",
     "/autores": "Entrei na Rotas Autores"
 };*/
 
-/*const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {       //a Criação do servidor também já foi substituido pelo framework exrpess!
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end(rotas[req.url]);
 });*/
--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
-app.get("/",(req,res) => {
-    res.status(200).send("Curso de Node.js");
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit"
+    }
+]
+
+app.get("/",(req,res) => {  //o get é método http que eu quero que ele faça!
+    res.status(200).send("Curso de Node.js"); //o send só é ultilizado para estruturas simples tipo uma string!
+});
+
+app.get("/livros", (req,res) => {
+    res.status(200).json(livros);  //já o json explicita que quer que retorne um arquivo json!
 });
 
 export default app;

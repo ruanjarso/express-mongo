3e7491785bcfcd451c84a3cd8975c3a2fb5f0cea
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-16 14:24:44 -0300

    create function cadastrarLivro with try-catch in controller

--- src/contrllers/livroController.js
+++ src/contrllers/livroController.js
@@ -5,6 +5,16 @@ class LivroController {
     static async listarLivros (req,res) {
         const listaLivros = await livro.find({});
         res.status(200).json(listaLivros);  //já o json explicita que quer que retorne um arquivo json!
+    };
+
+    static async cadastrarLivro (req,res) {
+        // O try catch serve para manejos de tentativas e falhas!
+        try {
+            const novoLivro = await livro.create(req,body);
+            res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+        } catch (error) {
+            res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro!` });
+        }
     }
 
 };

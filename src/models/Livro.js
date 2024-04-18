9e3ff67a4b7715d523c397558013ce1a10b856a9
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-18 18:43:35 -0300

    add coments

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const livrosSchema = new mongoose.Schema({
+const livrosSchema = new mongoose.Schema({  //definição do modelo que queremos que um livro seja!
     id:{ type: mongoose.Schema.Types.ObjectId },
     titulo: {type: String, required: true},
     editora: {type: String},
@@ -8,6 +8,7 @@ const livrosSchema = new mongoose.Schema({
     paginas: {type: Number}
 }, {versionKey: false});
 
-const livro = mongoose.model("livros", livrosSchema);
+const livro = mongoose.model("livros", livrosSchema); //"livros" <= refere-se a coleção e livroSchema refere-se ao modelo que declaramos logo a cima!
 
+//exportação do modelo para que outros arquivos interaja, com isso!
 export default livro;

1c0183bceea37e498e3a82f26ec5ea351bca04a0
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-20 11:53:43 -0300

    delete app.get for id, delete app.put and app.post in app.js

--- src/app.js
+++ src/app.js
@@ -16,22 +16,6 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.get("/livros/:id",(req,res) => { //O ":" em ":id" é para avisar o express que o "id" vai ser algo variável!
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-})
-
-/*app.post("/livros",(req,res) => { //O ".post" é para realizar o método POST do http que insere um dado!
-    livros.push(req.body);
-    res.status(201).send("livro cadastrado com sucesso!")
-})*/
-
-app.put("/livros/:id",(req,res) => { //O ".put" é para realizar o método PUT do http que atualiza os dados!
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-})
-
 app.delete("/livros/:id",(req,res) => { //O ".delete" é para realizar o método DELETE do http que deleta um dado!
     const index = buscaLivro(req.params.id);
     livros.splice(index, 1);   //O "splice" é um método javaScript que deleta um elemento de um array!

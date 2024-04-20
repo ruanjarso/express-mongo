79e88989f2cdc52050804c72c5951e33686e55fe
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-20 11:51:17 -0300

    create atualizarLivro in livroController.js

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -33,6 +33,16 @@ class LivroController {
         }
     }
 
+    static async atualizarLivro (req,res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json( {message: "livro atualizado!"} );  
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na atualização` } )
+        }
+    };
+
 };
 
 export default LivroController;

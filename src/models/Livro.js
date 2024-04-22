136a8aad2e790063f9f4c8952004c0e666503b01
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-22 15:22:55 -0300

    import autorSchema and add autor: autorSchema in Livro.js

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import { autorSchema } from "Autor.js";
 
 const livroSchema = new mongoose.Schema({  //definição do modelo que queremos que um livro seja!
     id:{ type: mongoose.Schema.Types.ObjectId },
     titulo: {type: String, required: true},
     editora: {type: String},
     preco: {type: Number},
-    paginas: {type: Number}
+    paginas: {type: Number},
+    autor: autorSchema
 }, {versionKey: false});
 
 const livro = mongoose.model("livros", livroSchema); //"livros" <= refere-se a coleção e livroSchema refere-se ao modelo que declaramos logo a cima!

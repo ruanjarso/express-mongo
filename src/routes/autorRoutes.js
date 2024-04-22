bfd54aace4b8a1fdfb310c278ceb53874b3939ae
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-22 15:13:19 -0300

    implement autorRoutes.js

--- src/routes/autorRoutes.js
+++ src/routes/autorRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import LivroController from "../controllers/livroController.js";
+import AutorController from "../controllers/autorController.js";
 
 const routes = express.Router()
 
-routes.get("/livros", LivroController.listarLivros);
-routes.get("/livros/:id", LivroController.listarLivroPorId);
-routes.post("/livros", LivroController.cadastrarLivro);
-routes.put("/livros/:id", LivroController.atualizarLivro);
-routes.delete("/livros/:id", LivroController.deletarLivro);
+routes.get("/autores", AutorController.listarAutores);
+routes.get("/autores/:id", AutorController.listarAutorPorId);
+routes.post("/autores", AutorController.cadastrarAutor);
+routes.put("/autores/:id", AutorController.atualizarAutor);
+routes.delete("/autores/:id", AutorController.deletarAutor);
 export default routes;

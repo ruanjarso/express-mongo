6277ee7fc30cdf50db9cafdc6c948037ffcdae5a
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-04-22 14:43:20 -0300

    add coments in livroController.js

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,29 +1,48 @@
 import livro from "../models/Livro.js";
 
-class LivroController {
+class LivroController { //classe na qual vamos exportar os métodos que estão nela!
+
+    static async listarLivros(req, res) { /* sobre o "static":  
+    Métodos Estáticos:
+    Definição: Um método estático é declarado usando a palavra-chave static.
+    Chamada: Os métodos estáticos não são chamados em instâncias da classe.
+    Em vez disso, eles são chamados diretamente na própria classe.
+    Utilidade: Geralmente, os métodos estáticos são funções utilitárias, 
+    como aquelas usadas para criar ou clonar objetos.
+
+    EXEMPLO:
+    class ExemploClasse {
+        static metodoEstatico() {
+        return "O método estático foi chamado";
+    }
+    }
+
+    // Chamada do método estático:
+    console.log(ExemploClasse.metodoEstatico());
     
-    static async listarLivros (req,res) {
+    Saída: "O método estático foi chamado"
+    */
 
         try {
             const listaLivros = await livro.find({});
             res.status(200).json(listaLivros);  //já o json explicita que quer que retorne um arquivo json!           
         } catch (erro) {
-            res.status(500).json({ message: `${erro.message} - falha na requisição` } )
+            res.status(500).json({ message: `${erro.message} - falha na requisição` })
         }
 
     };
 
-    static async listarLivroPorId (req,res) {
+    static async listarLivroPorId(req, res) {
         try {
             const id = req.params.id;
             const livroEncontrado = await livro.findById(id);
             res.status(200).json(livroEncontrado);  //já o json explicita que quer que retorne um arquivo json!           
         } catch (erro) {
-            res.status(500).json({ message: `${erro.message} - falha na requisição do livro` } )
+            res.status(500).json({ message: `${erro.message} - falha na requisição do livro` })
         }
     };
 
-    static async cadastrarLivro (req,res) {
+    static async cadastrarLivro(req, res) {
         // O try catch serve para manejos de tentativas e falhas!
         try {
             const novoLivro = await livro.create(req.body);
@@ -33,23 +52,23 @@ class LivroController {
         }
     }
 
-    static async atualizarLivro (req,res) {
+    static async atualizarLivro(req, res) {
         try {
             const id = req.params.id;
             await livro.findByIdAndUpdate(id, req.body);
-            res.status(200).json( {message: "livro atualizado!"} );  
+            res.status(200).json({ message: "livro atualizado!" });
         } catch (erro) {
-            res.status(500).json({ message: `${erro.message} - falha na atualização` } )
+            res.status(500).json({ message: `${erro.message} - falha na atualização` })
         }
     };
 
-    static async deletarLivro(req,res) {
+    static async deletarLivro(req, res) {
         try {
             const id = req.params.id;
             await livro.findByIdAndDelete(id);
-            res.status(200).json( {message: "livro deletado!"} );  
+            res.status(200).json({ message: "livro deletado!" });
         } catch (erro) {
-            res.status(500).json({ message: `${erro.message} - falha na exclusão!` } )
+            res.status(500).json({ message: `${erro.message} - falha na exclusão!` })
         }
     }
 
